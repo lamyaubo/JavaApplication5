c3948a2efa684289726d0bab11c5196c8c708cab
Lam Yau Bo <lamyaubo@gmail.com>
2021-03-11 15:38:45 +0800

    Add "World"

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -14,6 +14,6 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello!!");
+        System.out.println("Hello World!!");
     }
 }
